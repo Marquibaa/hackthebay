c2de3d0f9384c5f490f74402debafbd3494801f3
ZaraAlam29 <tanaazalam2@gmail.com>
2026-04-11 15:39:00 -0400

    dark mode

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -4,9 +4,9 @@ import MobileNav from "./MobileNav";
 
 const AppLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="flex min-h-screen bg-background">
+    <div className="flex min-h-screen bg-black text-white">
       <AppSidebar />
-      <div className="flex-1 flex flex-col min-h-screen">
+      <div className="h-full bg-black text-white border-r border-cyan-400 shadow-[0_0_25px_#00F0FF]">
         <MobileNav />
         <main className="flex-1 p-4 md:p-6 lg:p-8 overflow-auto">
           {children}
